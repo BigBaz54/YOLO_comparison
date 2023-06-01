b85a3f0d486af92f8919c5c86e2fc526de446921
BigBaz <louisvincent@famillecapelli.fr>
2023-06-01 11:48:56 +0200

    [+] more practical __call__()

--- v6/cpu/model_wrapper.py
+++ v6/cpu/model_wrapper.py
@@ -1,5 +1,6 @@
 import time
 import torch
+import os
 
 
 class ModelWrapper:
@@ -17,7 +18,7 @@ class ModelWrapper:
     def __str__(self):
         return self.name
     
-    def __call__(self, *args, **kwargs):
+    def __call__(self, save_dir=os.path.join('..', '..', 'runs', 'v6')):
         t = time.time()
-        self.inferer.run(*args, **kwargs)
+        self.inferer.infer(0.25, 0.45, None, False, 1000, save_dir, False, True, False, False, False)
         self.detection_time = time.time() - t
