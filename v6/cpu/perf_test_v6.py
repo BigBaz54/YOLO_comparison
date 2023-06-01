382924dc4def6408f919ff3b4c60cd9df114d9a0
BigBaz <louisvincent@famillecapelli.fr>
2023-06-01 14:59:51 +0200

    [~] fixing imports and working dir

--- v6/cpu/perf_test_v6.py
+++ v6/cpu/perf_test_v6.py
@@ -8,7 +8,6 @@ sys.path.append(os.path.join('v6', 'yolov6_main'))
 from yolov6.core.inferer import Inferer
 
 
-os.chdir(os.path.join('v6', 'yolov6_main'))
 
 @torch.no_grad()
 def get_inferer(weights, size=640):
@@ -17,6 +16,7 @@ def get_inferer(weights, size=640):
 def load_models():
     models = []
 
+    os.chdir(os.path.join('v6', 'yolov6_main')) 
     models.append(ModelWrapper(get_inferer(os.path.join('..', 'models', 'yolov6n.pt')), 'yolov6n'))
     models.append(ModelWrapper(get_inferer(os.path.join('..', 'models', 'yolov6s.pt')), 'yolov6s'))
     models.append(ModelWrapper(get_inferer(os.path.join('..', 'models', 'yolov6m.pt')), 'yolov6m'))
@@ -25,16 +25,19 @@ def load_models():
     models.append(ModelWrapper(get_inferer(os.path.join('..', 'models', 'yolov6s6.pt'), size=640), 'yolov6s6', size=640))
     models.append(ModelWrapper(get_inferer(os.path.join('..', 'models', 'yolov6m6.pt'), size=640), 'yolov6m6', size=640))
     models.append(ModelWrapper(get_inferer(os.path.join('..', 'models', 'yolov6l6.pt'), size=640), 'yolov6l6', size=640))
+    os.chdir(os.path.join('..', '..'))
 
     return models
 
 def perf_test(models):
-    img_nb = len(os.listdir(os.path.join("..", "..", "img")))
+    img_nb = len(os.listdir('img'))
     print(f'\n\n>>> YOLOv6 : Run inference on {img_nb} images <<<\n')
+    os.chdir(os.path.join('v6', 'yolov6_main'))
     for model in models:
         model()
         print(f'{model.name} ({model.size}x{model.size}) - {round(model.detection_time, 3)}s - {round(img_nb/model.detection_time, 3)} FPS')
         # result.save()
+    os.chdir(os.path.join('..', '..'))
 
 if __name__=="__main__":
     models = load_models()
