f3beeec517509fdd25caa17e84e1b378ae3ad22c
BigBaz <louisvincent@famillecapelli.fr>
2023-06-01 11:37:43 +0200

    [+] temp now loads inferer then runs (allows to measure time)

--- v6/cpu/perf_test_temp.py
+++ v6/cpu/perf_test_temp.py
@@ -1,7 +1,14 @@
+import torch
 import os
-
 import sys
+
 sys.path.append(os.path.join('v6', 'yolov6-main'))
-import tools.infer as infer
+from yolov6.core.inferer import Inferer
 os.chdir(os.path.join('v6', 'yolov6-main'))
-infer.run(os.path.join('..', 'models', 'yolov6s.pt'), os.path.join('..', '..', 'img'), yaml=os.path.join('data', 'coco.yaml'))
+@torch.no_grad()
+def get_inferer():
+    return Inferer(os.path.join('..', '..', 'img'), False, 0, os.path.join('..', 'models', 'yolov6s.pt'), 0, os.path.join('data', 'coco.yaml'), 640, False)
+
+inferer = get_inferer()
+inferer.infer(0.25, 0.45, None, False, 1000, os.path.join('..', '..', 'runs', 'v6'), False, True, False, False, False)
+# infer.run(os.path.join('..', 'models', 'yolov6s.pt'), os.path.join('..', '..', 'img'), yaml=os.path.join('data', 'coco.yaml'))
