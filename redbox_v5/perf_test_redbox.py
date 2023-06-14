fb910163d2e2b0bfeee4ab7abc9040a3e390e5a2
BigBaz <louisvincent@famillecapelli.fr>
2023-06-14 10:31:06 +0200

    [+] mobile models running

---
+++ redbox_v5/perf_test_redbox.py
@@ -0,0 +1,70 @@
+import sys
+import os
+sys.path.append(os.path.join('.'))
+try:
+    from redbox_v5.model_wrapper_redbox import ModelWrapper
+except ModuleNotFoundError:
+    print("Please run the file from the root of the repository.")
+    exit(1)
+
+
+import torch
+import os
+import cv2
+import platform
+import GPUtil
+
+
+def load_models():
+    models = []
+    
+    models.append(ModelWrapper(torch.hub.load('ultralytics/yolov5', 'custom', path=os.path.join('redbox_v5', 'models', 'v5s160_fit_within.pt'), force_reload=True), 'v5s160', size=160))
+    models.append(ModelWrapper(torch.hub.load('ultralytics/yolov5', 'custom', path=os.path.join('redbox_v5', 'models', 'v5s320_fit_within.pt'), force_reload=True), 'v5s320', size=320))
+    models.append(ModelWrapper(torch.hub.load('ultralytics/yolov5', 'custom', path=os.path.join('redbox_v5', 'models', 'v5s640.pt'), force_reload=True), 'v5s640', size=640))
+
+    for model in models:
+        model.eval()
+    return models
+
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image()
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+    
+    resized = cv2.resize(image, dim, interpolation = inter)
+    return resized
+
+def image_preprocess(image, target_size):
+    img = cv2.imread(image)
+    img = image_resize(img, target_size, target_size)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
+
+def perf_test(models):
+    sizes = [160, 320, 640]
+
+    imgs = [os.path.join('img', img) for img in os.listdir('img') if img.endswith('.jpg') or img.endswith('.png') or img.endswith('.jpeg')]
+    imgs_by_size = {}
+    for size in sizes:
+        imgs_preprocessed = [image_preprocess(img, size) for img in imgs]
+        imgs_by_size[size] = imgs_preprocessed
+    print(f'\n\nCPU: {platform.processor()}')
+    print(f'GPUs: {[gpu.name for gpu in GPUtil.getGPUs()]}')
+    print(f'\n>>>>> YOLOv5 : Run inference on {len(imgs)} images <<<<<\n')
+    for model in models:
+        imgs_copy = [img.copy() for img in imgs_by_size[model.size]]
+        result = model(imgs_copy, size=model.size)
+        print(f'{f"{model.name} " + f"({model.size}x{model.size})":>25} - {round(model.detection_time, 3):>7}s - {round(len(imgs)/model.detection_time, 3):>6} FPS')
+        # result.save()
+
+if __name__=="__main__":
+    models = load_models()
+    perf_test(models)
