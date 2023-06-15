b63574a1c93e347818da712b3559070e46b6d9b1
BigBaz <louisvincent@famillecapelli.fr>
2023-06-15 10:30:10 +0200

    [~] fix out of range when no more changes in nb_objects

--- redbox_v5/perf_test_redbox.py
+++ redbox_v5/perf_test_redbox.py
@@ -110,7 +110,7 @@ def perf_test_vid(models, video_name, confidence=0.5, max_frames=None):
     time_step = frame_total/video_fps/1000
     time = 0.494677
     while (video.isOpened() and ((max_frames is None) or (frame_done < max_frames))):
-        if time > nb_objects_evolution[0][0]:
+        if time > nb_objects_evolution[0][0] and len(nb_objects_evolution) > 0:
             nb_objects = nb_objects_evolution[0][1]
             nb_objects_evolution.pop(0)
         ret, frame = video.read()
@@ -139,7 +139,7 @@ def perf_test_vid(models, video_name, confidence=0.5, max_frames=None):
         results[model.name].release()
 
 if __name__=="__main__":
-    # models = load_models()
+    models = load_models()
     # perf_test_img(models)
-    # perf_test_vid(models, 'cam05.mp4')
-    print(get_nb_objects_evolution('cam05.mp4'))
+    perf_test_vid(models, 'cam05.mp4')
+    # print(get_nb_objects_evolution('cam05.mp4'))
