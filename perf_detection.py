aafbfae4d00966f03782de6e72e76da8bf68e34b
BigBaz <louisvincent@famillecapelli.fr>
2023-06-22 10:51:42 +0200

    [~] computing metrics over all the frames (not just 1)

--- perf_detection.py
+++ perf_detection.py
@@ -1,33 +1,44 @@
-def get_metrics(gt_file, detections):
-    gt = parse_gt(gt_file)
+def get_metrics(gt_file, all_detections):
+    ground_truth = parse_gt(gt_file)
 
-    # Build a list of all the (gt, det) pairs
-    gt_det_pairs = []
-    for det in detections:
-        for g in gt:
-            # If the class is the same, add the pair to the list
-            if g['class_id'] == det['class_id']:
-                gt_det_pairs.append((g, det))
-    
-    gt_get_pairs_with_iou = []
-    for pair in gt_det_pairs:
-        pair_iou = iou(pair[0], pair[1])
-        if pair_iou > 0.5:
-            gt_get_pairs_with_iou.append((pair, pair_iou))
-    
-    # Sort the pairs by decreasing IOU
-    gt_get_pairs_with_iou.sort(key=lambda x: x[1], reverse=True)
+    # The actual number of frames in the video
+    nb_frames = len(all_detections)
 
-    # Count the number of true positives
-    tp = 0
-    while len(gt_get_pairs_with_iou) > 0:
-        tp += 1
-        # Remove the pair with the highest IOU from the list and remove all the pairs that have the same det
-        gt_get_pairs_with_iou = [pair for pair in gt_get_pairs_with_iou if pair[0][1] != gt_get_pairs_with_iou[0][0][1]]
+    # Keep only the last nb_frames frames of the ground truth 
+    ground_truth = ground_truth[-nb_frames:]
+
+    for i in range(nb_frames):
+        gt = ground_truth[i]
+        detections = all_detections[i]
+        # Build a list of all the (gt, det) pairs
+        gt_det_pairs = []
+        for det in detections:
+            for g in gt:
+                # If the class is the same, add the pair to the list
+                if int(g['class_id']) == int(det['class_id']):
+                    gt_det_pairs.append((g, det))
+        
+        gt_get_pairs_with_iou = []
+        for pair in gt_det_pairs:
+            pair_iou = iou(pair[0], pair[1])
+            if pair_iou > 0.5:
+                gt_get_pairs_with_iou.append((pair, pair_iou))
+        
+        # Sort the pairs by decreasing IOU
+        gt_get_pairs_with_iou.sort(key=lambda x: x[1], reverse=True)
+
+        # Count the number of true positives
+        tp = 0
+        while len(gt_get_pairs_with_iou) > 0:
+            tp += 1
+            # Remove the pair with the highest IOU from the list and remove all the pairs that have the same det
+            gt_get_pairs_with_iou = [pair for pair in gt_get_pairs_with_iou if pair[0][1] != gt_get_pairs_with_iou[0][0][1]]
     
     # Compute the metrics
-    fp = len(detections) - tp
-    fn = len(gt) - tp   
+    nb_detections = sum([len(detections) for detections in all_detections])
+    nb_gt = sum([len(gt) for gt in ground_truth])
+    fp = nb_detections - tp
+    fn = nb_gt - tp  
     precision = tp / (tp + fp)
     recall = tp / (tp + fn)
     if precision + recall == 0:
@@ -55,18 +66,18 @@ def iou(bbox1, bbox2):
 def parse_gt(gt_file):
     # ne pas oublier de faire 1 - y pour bottom et top
     return [
-        {'class_id': 0, 'left': 0.1, 'bottom': 0.2, 'right': 0.2, 'top': 0.1},
+        [{'class_id': 0, 'left': 0.1, 'bottom': 0.2, 'right': 0.2, 'top': 0.1}],
     ]
 
 
 if __name__ == '__main__':
     gt_file = 'gt.txt'
     detections = [
-        {'class_id': 0, 'left': 0.1, 'bottom': 0.2, 'right': 0.2, 'top': 0.1},
+        [{'class_id': 0, 'left': 0.1, 'bottom': 0.2, 'right': 0.2, 'top': 0.1},
         {'class_id': 0, 'left': 0.2, 'bottom': 0.3, 'right': 0.3, 'top': 0.2},
         {'class_id': 0, 'left': 0.3, 'bottom': 0.4, 'right': 0.4, 'top': 0.3},
         {'class_id': 0, 'left': 0.4, 'bottom': 0.5, 'right': 0.5, 'top': 0.4},
-        {'class_id': 0, 'left': 0.5, 'bottom': 0.6, 'right': 0.6, 'top': 0.5},
+        {'class_id': 0, 'left': 0.5, 'bottom': 0.6, 'right': 0.6, 'top': 0.5}],
     ]
     metrics = get_metrics(gt_file, detections)
     print(metrics)
