84c05d0f4edf313dd98f68918b61d0e63388b17c
BigBaz <louisvincent@famillecapelli.fr>
2023-06-26 16:12:01 +0200

    [~] fix iou and plot_iou for debug

--- perf_detection.py
+++ perf_detection.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 def get_metrics(gt_file, all_detections):
     ground_truth = parse_gt(gt_file)
 
@@ -13,15 +15,21 @@ def get_metrics(gt_file, all_detections):
         # Build a list of all the (gt, det) pairs
         gt_det_pairs = []
         for det in detections:
+            print()
+            print()
             for g in gt:
+                print('gt:', g)
+                print('det:', det)
+                print()
                 # If the class is the same, add the pair to the list
                 if int(g['class_id']) == int(det['class_id']):
                     gt_det_pairs.append((g, det))
         
         gt_det_pairs_with_iou = []
         for pair in gt_det_pairs:
+            plot_iou(pair[0], pair[1])
             pair_iou = iou(pair[0], pair[1])
-            if pair_iou > 0.5:
+            if pair_iou > 0.3:
                 gt_det_pairs_with_iou.append((pair, pair_iou))
         
         # Sort the pairs by decreasing IOU
@@ -50,16 +58,16 @@ def get_metrics(gt_file, all_detections):
 def iou(bbox1, bbox2):
     # Compute the intersection
     x1 = max(bbox1['left'], bbox2['left'])
-    y1 = max(bbox1['bottom'], bbox2['bottom'])
+    y1 = max(bbox1['top'], bbox2['top'])
     x2 = min(bbox1['right'], bbox2['right'])
-    y2 = min(bbox1['top'], bbox2['top'])
-    intersection = max(0, x2 - x1) * max(0, y1 - y2)
+    y2 = min(bbox1['bottom'], bbox2['bottom'])
+    intersection = max(0, x2 - x1) * max(0, y2 - y1)
 
     # Compute the union
     area1 = (bbox1['right'] - bbox1['left']) * (bbox1['bottom'] - bbox1['top'])
     area2 = (bbox2['right'] - bbox2['left']) * (bbox2['bottom'] - bbox2['top'])
     union = area1 + area2 - intersection
-    
+
     # Compute the IOU
     return intersection / union
 
@@ -92,7 +100,32 @@ def parse_gt(gt_file):
                 })
     return gt_by_frame
 
+def plot_iou(bbox1, bbox2):
+    plt.figure()
+
+    #  Invert the y axis
+    plt.gca().invert_yaxis()
+
+    # Set the limits of the plot
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
+
+    # Plots bbox1
+    plt.plot([bbox1['left'], bbox1['right']], [bbox1['top'], bbox1['top']], color='black')
+    plt.plot([bbox1['left'], bbox1['right']], [bbox1['bottom'], bbox1['bottom']], color='black')
+    plt.plot([bbox1['left'], bbox1['left']], [bbox1['top'], bbox1['bottom']], color='black')
+    plt.plot([bbox1['right'], bbox1['right']], [bbox1['top'], bbox1['bottom']], color='black')
+
+    # Plots bbox2
+    plt.plot([bbox2['left'], bbox2['right']], [bbox2['top'], bbox2['top']], color='red')
+    plt.plot([bbox2['left'], bbox2['right']], [bbox2['bottom'], bbox2['bottom']], color='red')
+    plt.plot([bbox2['left'], bbox2['left']], [bbox2['top'], bbox2['bottom']], color='red')
+    plt.plot([bbox2['right'], bbox2['right']], [bbox2['top'], bbox2['bottom']], color='red')
+
+    # Show the plot
+    plt.show()
+
 
 if __name__ == '__main__':
-    gt_file = 'cam11start.txt'
-    print(len(parse_gt(gt_file)))
+    # print(iou({'left': 0.5, 'top': 0.5, 'right': 0.7, 'bottom': .8}, {'left': 0.5, 'top': 0.5, 'right': 0.75, 'bottom': 0.75}))
+    plot_iou({'class_id': 0, 'left': 0.54, 'top': 0.43999999999999995, 'right': 0.59, 'bottom': 0.53}, {'class_id': 0, 'confidence': 0.8829478621482849, 'left': 0.5443516254425049, 'top': 0.2520040988922119, 'right': 0.5868278980255127, 'bottom': 0.2959097385406494})
